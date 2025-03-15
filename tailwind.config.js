5097461566ef35d61384c047829d4a694a8f5b49
magdyMohammed20 <magdymohammed37@yahoo.com>
2025-03-15 09:00:53 +0200

    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,19 @@ export default {
                 "card": "-2.5em 3.125em 5.25em rgba(0,0,0,0.1)",
             },
             colors: {
-          bage:'#f4eeeaf6',
-        }
+              bage: '#f4eeeaf6',
+              'bage-light': '#fcf9f7'
+        },
+            margin: {
+        '60px': '60px',
+        },
+   fontSize: {
+        'tiny': '0.85em'
+        },
+   fontFamily: {
+        customFont: ['FS Neruda Light'],
+        // Add more custom font families as needed
+      },
         },
     },
     plugins: [],
